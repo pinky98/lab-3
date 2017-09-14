9e60f5121a372e1762b7f71dee4862110d4c44d9
pinky98 <31351080+pinky98@users.noreply.github.com>
2017-09-14 16:03:28 +0530

    Add files via upload

---
+++ lab3 question 23.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+ 
+int a,n;
+cout<<"enter the value of n\n";
+cin>>n;
+while(1<=n){
+cout<<" "<<n;
+n--;
+}
+	// your code goes here
+	return 0;
+}
+
