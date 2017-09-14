8a4e97326ae1608f4fbc0a8aa782b641d5eb4850
pinky98 <31351080+pinky98@users.noreply.github.com>
2017-09-14 15:57:36 +0530

    Add files via upload

---
+++ lab 3 question 41.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	
+	char ch;
+	int i;
+	for(i=1; i<255; i++)
+	{
+		ch=i;
+		cout<<i<<"-> "<<ch<<"\t";
+	}
+	// your code goes here
+	return 0;
+}
