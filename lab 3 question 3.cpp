8dc0b55e9792bf9b63f661ba360d70f0b55bae1d
pinky98 <31351080+pinky98@users.noreply.github.com>
2017-09-14 14:42:30 +0530

    Add files via upload

---
+++ lab 3 question 3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cout<<"enter the number"<<endl;
+	cin>>n;
+	if(n>0)
+	cout<<" the number is positive";
+	else if(n<0)
+	cout<<"the number is negative";
+	else
+	cout<<"the number is zero";
+	// your code goes here
+	return 0;
+}
