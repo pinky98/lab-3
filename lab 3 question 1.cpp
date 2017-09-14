5c1b74e18fa1e279daade6ad862c76f6314054d2
pinky98 <31351080+pinky98@users.noreply.github.com>
2017-09-14 14:41:43 +0530

    Add files via upload

---
+++ lab 3 question 1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a,b;
+	cout<<"enter the first number";
+	cin>>a;
+	cout<<"enter the second number";
+	cin>>b;
+	if(a>b)
+	cout<<a <<"is maximum";
+	else
+	cout<<b<<"is maximum";
+	// your code goes here
+	return 0;
+}
